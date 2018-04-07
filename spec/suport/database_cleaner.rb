0a47203773b96db21482cc7427b56608138d0c07
Pedro Custódio <pedro@dogme.com.br>
2018-04-07 16:17:24 -0300

    Add and adjusting in database clear

--- spec/suport/database_cleaner.rb
+++ spec/suport/database_cleaner.rb
@@ -1,24 +1,14 @@
 RSpec.configure do |config|
 
-  config.use_transactional_fixtures = false
-
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner[:mongoid].strategy = :truncation
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
+    DatabaseCleaner[:mongoid].start
   end
 
   config.after(:each) do
-    DatabaseCleaner.clean
+    DatabaseCleaner[:mongoid].clean
   end
 end
