825d8417bc9a1eb33c6a5f8f33aa2166aa993abb
Pedro Custódio <pedro@dogme.com.br>
2018-04-07 15:59:26 -0300

    Validates presence from attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,10 @@ class Product
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
-  field :id, type: Integer
   field :height, type: Float
   field :weight, type: Float
   field :width, type: Float
   field :length, type: Float
+
+  validates :name, :height, :weight, :width, :length, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is not valid without a name, must have a specific message error" do
+    product = Product.new(name: nil)
+    expect(product).to_not be_valid
+    expect(product.errors[:name]).to include("can't be blank")
+  end
+  
+  it "is not valid without a height, must have a specific message error" do
+    product = Product.new(height: nil)
+    expect(product).to_not be_valid
+    expect(product.errors[:height]).to include("can't be blank")
+  end
+
+  it "is not valid without a weight, must have a specific message error" do
+    product = Product.new(weight: nil)
+    expect(product).to_not be_valid
+    expect(product.errors[:weight]).to include("can't be blank")
+  end
+
+  it "is not valid without a width, must have a specific message error" do
+    product = Product.new(width: nil)
+    expect(product).to_not be_valid
+    expect(product.errors[:width]).to include("can't be blank")
+  end
+
+  it "must be valid with values of attributes presence" do
+    product = Product.new(name: FFaker::Product.product,
+                          height: rand(0..1000).to_f,
+                          weight: rand(0..1000).to_f,
+                          width: rand(0..1000).to_f,
+                          length: rand(0..1000).to_f)
+    expect(product).to be_valid
+  end
+
+  it "must be valid without values of attributes presence" do
+    product = Product.new(name: nil,
+                          height: rand(0..1000).to_f,
+                          weight: rand(0..1000).to_f,
+                          width: rand(0..1000).to_f,
+                          length: rand(0..1000).to_f)
+    expect(product).not_to be_valid
+    expect(product.errors[:name]).to include("can't be blank")
+  end
 end
