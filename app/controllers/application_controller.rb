e5939561de796a3fde6a6dbdf88590b9ec45cacf
Pedro Custódio <pedro@dogme.com.br>
2018-04-08 14:47:37 -0300

    Custom message of record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include Response
+
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :document_not_found
+  
+  def document_not_found
+    json_response({ message: 'Não há resultados para esse id, tente outro!' }, :not_found)
+  end
 end
